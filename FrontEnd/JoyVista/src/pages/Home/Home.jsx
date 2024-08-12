7584acdd4f849b56535ec7db0f0a913eec1b5cca
abhishek lohani <alohani1308@gmail.com>
2024-08-13 00:14:38 +0530

    handle some exp on Home

--- FrontEnd/JoyVista/src/pages/Home/Home.jsx
+++ FrontEnd/JoyVista/src/pages/Home/Home.jsx
@@ -1,16 +1,34 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from 'react'
 import { useFetch } from '../../hooks/useFetch';
+import Loader from '../../components/Loader/Loader';
+import { fetchfromapi, sendDataToapi } from '../../utils/api';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+  const navigate = useNavigate();
   const [user,setuser] = useState(null);
   const {data} = useFetch("/users/loggedin-user");
   useEffect(()=>{
     setuser(data);
   },[data]);
   
+  const [loading,setloading] = useState(false);
+  const handleLogout = ()=>{
+    setloading(true);
+    sendDataToapi("/users/logout").then((res)=>{
+      setloading(false);
+      console.log(res);
+      window.location.reload();
+    }).catch((err)=>{
+      setloading(false);
+      console.log(err);
+    })
+  }
+
   return (
-    <>{user && <div>Home</div>}{!user && <div>Please Login</div>}</>
+    <>{user && <div>Home <button onClick={handleLogout}>Logout</button>{loading && 
+    <Loader />}</div>}{!user && <div>Please Login <button onClick={()=>{navigate("/login")}}>Login</button></div>}</>
   )
 }
 
