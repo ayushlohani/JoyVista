8dcbb755ce6f4ab6f38ece155608399fbd33c7e4
ayushlohani <125081866+ayushlohani@users.noreply.github.com>
2025-02-06 13:08:11 +0530

    Update connectDb.js

--- BackEnd/src/db/connectDb.js
+++ BackEnd/src/db/connectDb.js
@@ -3,7 +3,7 @@ import { db_name } from "../constants.js"
 
 const connectDb =async ()=>{
     try {
-        const dbConnectInstance = await mongoose.connect(`mongodb+srv://lohaniayush97:ayush123@cluster0.pmjawtc.mongodb.net/joyvista`);
+        const dbConnectInstance = await mongoose.connect(`mongodb+srv://lohaniayush97:<password>@cluster0.pmjawtc.mongodb.net/<db_name>`);
         console.log(`DB Connected Succesfully at ${dbConnectInstance.connection.host}`)
     } catch (error) {
         console.log(error.message);
